fa978e5be6de3f6a5a22b2764441829cc82e87a1
dawood-shah64 <helexalpha334@gmail.com>
2025-04-27 00:37:10 +0530

    Add files via upload

---
+++ taks_2.py
@@ -0,0 +1,4 @@
+frist_name = str(input("Enter the first name: "))
+last_name = str(input("Enter the last name: "))
+
+print("hello,",frist_name,last_name,'!'" welcome to python program.")
---
+++ task_1.py
@@ -0,0 +1,9 @@
+NO1 = int(input("enter first number: "))
+NO2 = int(input("enter second number: "))
+
+add = NO1 + NO2
+sub = NO1 - NO2
+mul = NO1 * NO2
+div = NO1/NO2
+
+print("Addition:",add,"\nsubtraction:",sub, "\nmultiplication:",mul,"\ndivision:",div,)
